858e95725c9fe054caa859a2b38a2c57b4bdb701
HARSH KUMAR VIMAL <hvimal605@gmail.com>
2024-07-13 15:02:06 +0530

    commit latest

--- Backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-require('dotenv').config()
-exports.dbConnect = ()=>{
-    mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>console.log("db connected successfully"))
-    .catch((error)=>console.log(error))
-}
-
