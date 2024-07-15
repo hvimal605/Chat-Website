cdd638f8f5ee6865467f29d5de54c3b578202bf8
HARSH KUMAR VIMAL <hvimal605@gmail.com>
2024-07-15 15:33:41 +0530

    commit latest

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const user = require('./routes/user');
-const message = require('./routes/message');
+const userRoutes = require('./routes/user');
+const messageRoutes = require('./routes/message');
 const { app, server } = require('./socketIO/server'); 
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
-
 const db = require('./config/database');
-const { cloudinaryConnect } = require('./config/clodinary');
+const { cloudinaryConnect } = require('./config/cloudinary');
 require("dotenv").config();
 
 // Connect to database
@@ -17,47 +16,37 @@ db.dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// File upload middleware
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: '/tmp',
-  })
-);
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp',
+}));
 
 // Cloudinary setup
 cloudinaryConnect();
 
 // CORS configuration
-app.use(
-  cors({
-      origin: function (origin, callback) {
-          const allowedOrigins = [
-            "https://harshspark-chat-web.netlify.app"
-          ];
-          if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-              callback(null, true);
-          } else {
-              callback(new Error('Not allowed by CORS'));
-          }
-      },
-      credentials: true,
-  })
-);
-
-
+const allowedOrigins = ["https://harshspark-chat-web.netlify.app"];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 // Routes
 app.get('/', (req, res) => {
   res.send('<h1>Server is running up</h1>');
 });
 
-app.use("/api/user", user);
-app.use("/api/message", message);
+app.use("/api/user", userRoutes);
+app.use("/api/message", messageRoutes);
 
 // Start server
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
--- Backend/socketIO/server.js
+++ Backend/socketIO/server.js
@@ -5,43 +5,43 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "https://harshspark-chat-web.netlify.app",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "https://harshspark-chat-web.netlify.app",
+    methods: ["GET", "POST"],
+  },
 });
 
 const users = {};
 
 // Function to get recipient's socket ID
-const getRececiverSocketId = (receiverId) => {
-    return users[receiverId];
+const getReceiverSocketId = (receiverId) => {
+  return users[receiverId];
 };
 
 io.on("connection", (socket) => {
-    console.log('A user connected', socket.id);
-    const userId = socket.handshake.query.userId;
+  console.log('A user connected', socket.id);
+  const userId = socket.handshake.query.userId;
 
-    if (userId) {
-        users[userId] = socket.id;
-        console.log("Hello", users);
+  if (userId) {
+    users[userId] = socket.id;
+    console.log("Hello", users);
+  }
+
+  io.emit('getOnlineUsers', Object.keys(users));
+
+  socket.on('typing', (data) => {
+    const { senderId, receiverId, isTyping } = data;
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit('typing', { senderId, isTyping });
     }
+  });
 
-    io.emit('getOnlineUsers', Object.keys(users));
-
-    socket.on('typing', (data) => {
-        const { senderId, receiverId, isTyping } = data;
-        const receiverSocketId = getRececiverSocketId(receiverId);
-        if (receiverSocketId) {
-            io.to(receiverSocketId).emit('typing', { senderId, isTyping });
-        }
-    });
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected", socket.id);
-        delete users[userId];
-        io.emit("getOnlineUsers", Object.keys(users));
-    });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete users[userId];
+    io.emit("getOnlineUsers", Object.keys(users));
+  });
 });
 
-module.exports = { app, io, server, getRececiverSocketId };
+module.exports = { app, io, server, getReceiverSocketId };
